643e918583c2d4e7340ba71936120e79ace43366
Erfanmah7 <efn8mah@yahoo.com>
2024-06-01 02:56:57 +0330

    add Interceptor in Config

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import Post from "./components/Post";
 import Delete from "./components/Delete";
 import Patch from "./components/Patch";
 import Put from "./components/Put";
+import api from "./services/Config";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    axios
+    api
       .get("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => setTodos(res.data));
+      .then((res) => setTodos(res));
 
     axios
       .all([
