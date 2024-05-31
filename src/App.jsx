ab4b3a0981e25de2ca50192226ff63800fd1c54a
Erfanmah7 <efn8mah@yahoo.com>
2024-06-01 02:42:17 +0330

    all for multiple requests at the same time

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,20 @@ function App() {
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
       .then((res) => setTodos(res.data));
+
+    axios
+      .all([
+        axios.get("https://jsonplaceholder.typicode.com/todos"),
+        axios.get("https://jsonplaceholder.typicode.com/posts"),
+        axios.get("https://jsonplaceholder.typicode.com/users"),
+      ])
+      .then(
+        axios.spread((todos, posts, users) => {
+          console.log(todos.data);
+          console.log(posts.data);
+          console.log(users.data);
+        })
+      );
   }, []);
 
   return (
